3f4a9d4d2a1fab74e2e1df7d28cddceb8e213b05
Abhishek Gupta <hi@itsabhishek.me>
2020-08-20 18:09:16 +0530

    Updates

---
+++ precache-manifest.658a703170821663e5986178c82dca8b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3048fb6d451429547ede",
+    "url": "/cb-junior-admin/static/js/main.d7c94a3a.chunk.js"
+  },
+  {
+    "revision": "20ac02457a0faf5b6d4c",
+    "url": "/cb-junior-admin/static/js/runtime~main.aa97a526.js"
+  },
+  {
+    "revision": "1fcad9faf178900b6967",
+    "url": "/cb-junior-admin/static/js/2.6831b503.chunk.js"
+  },
+  {
+    "revision": "77844ac99c7cee6dad0063d23b32f61b",
+    "url": "/cb-junior-admin/index.html"
+  }
+];
